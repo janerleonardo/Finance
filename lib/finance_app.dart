e8c6ec56ad494a7d7f29ef61ad5e2272c332623f
Jan3r <janerleonardo@gmail.com>
2023-03-15 18:41:21 -0500

    Create Home Page

---
+++ lib/finance_app.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class FinanceApp extends StatelessWidget {
+  
+  
+    Widget _bottomAction(IconData icon) {
+      return InkWell(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(icon),
+        ),
+        onTap: () {},
+      );
+  }
+
+   Widget _body() {
+    return SafeArea(
+      child: Column(
+        children: [
+          _selector(),
+          _expenses(),
+          _graph(),
+          _list(),
+        ],
+      )
+       );
+  }
+  
+    _selector() => Container();
+    
+    _expenses() {
+      return Column(
+        children: [
+          Text("\$2361,41",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 40.0,
+            
+          ),
+          ),
+          Text("Total expenses",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16.0,
+            color: Colors.blueGrey,
+          ),)
+        ],
+      );
+    }
+  
+    _graph() => Container();
+    
+    _list() => Container();
+  @override
+  Widget build(BuildContext context) {
+
+    final _buttomBar = BottomAppBar(
+        notchMargin: 4.0,
+        shape: CircularNotchedRectangle(),
+        child: Row(
+          
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+           _bottomAction(FontAwesomeIcons.history),
+           _bottomAction(FontAwesomeIcons.chartPie),
+           SizedBox(width: 48.0),
+           _bottomAction(FontAwesomeIcons.wallet),
+           _bottomAction(Icons.settings),
+          ],
+        ),
+      );
+
+
+
+        return Scaffold(
+          bottomNavigationBar:
+           _buttomBar,
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: (){},
+            ),
+           body: _body(),
+        );
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-import 'bottom_bar.dart';
+import 'finance_app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -15,26 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FinanceApp(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-   
-   
-    return buttomBar();
-  }
-}
